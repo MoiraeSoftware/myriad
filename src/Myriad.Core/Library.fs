90c0f08e8f506181a380702799afe33335bc2b37
Enrico Sada <enrico@sada.io>
2019-10-06 20:01:42 +0200

    add core and example plugin

---
+++ src/Myriad.Core/Library.fs
@@ -0,0 +1,9 @@
+﻿namespace Myriad.Core
+
+open System
+
+type MyriadGenAttribute(generator: string) =
+    inherit Attribute()
+
+type IMyriadGen =
+    abstract member GetName: unit -> string
---
+++ src/Myriad.Plugins.Example1/Library.fs
@@ -0,0 +1,7 @@
+﻿namespace Myriad.Plugins.Example1
+
+open Myriad.Core
+
+type Example1Gen() =
+    interface IMyriadGen with
+        member __.GetName() = "test1"
