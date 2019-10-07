78a744eb455056eb7fd714858e799859b336b2eb
Dave Thomas <kukulcanenator@googlemail.com>
2019-10-07 14:13:52 +0100

    Add generator property to attribute

--- src/Myriad.Core/Library.fs
+++ src/Myriad.Core/Library.fs
@@ -4,7 +4,8 @@ open System
 
 type MyriadGenAttribute(generator: string) =
     inherit Attribute()
-
+    
+    member __.Generator = generator
 
 type MyriadSdkGeneratorAttribute(name: string) =
     inherit Attribute()
