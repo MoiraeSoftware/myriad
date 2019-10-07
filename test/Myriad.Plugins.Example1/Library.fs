1e5f31a65c5ced2332f5562c3d69c2f31a2bdc2b
Dave Thomas <kukulcanenator@googlemail.com>
2019-10-07 18:38:19 +0100

    Add implementation
    currently invalid module, needs an expression within

--- test/Myriad.Plugins.Example1/Library.fs
+++ test/Myriad.Plugins.Example1/Library.fs
@@ -1,8 +1,13 @@
 ﻿namespace Myriad.Plugins.Example1
 
 open Myriad.Core
+open Microsoft.FSharp.Compiler.Ast
+open FsAst
 
 [<MyriadSdkGenerator("example1")>]
 type Example1Gen() =
     interface IMyriadGen with
-        member __.DoThings() = "ok"
+        member __.Generate(namespace', ast) =
+            let ident = LongIdentWithDots.CreateString "test"
+            let lid = ident.Lid
+            SynModuleOrNamespaceRcd.CreateModule(lid)
