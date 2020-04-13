0f00c49a99cac818af74d84a1953bca2bd086964
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2020-04-13 18:42:05 +0200

    Add more tests

--- test/Myriad.IntegrationPluginTests/Tests.fs
+++ test/Myriad.IntegrationPluginTests/Tests.fs
@@ -2,6 +2,7 @@ module Tests
 
 open System
 open Xunit
+open Example
 
 [<Fact>]
 let ``Test namespace generated`` () =
@@ -10,3 +11,25 @@ let ``Test namespace generated`` () =
 [<Fact>]
 let ``Test2 namespace generated`` () =
     Assert.Equal(42, Test2.example1.fourtyTwo)
+
+[<Fact>]
+let ``Test1 create Test`` () =
+    let t = Test.Test1.create 1 "2" 3. (float32 4)
+    Assert.Equal({Test1.one = 1; two = "2"; three = 3.; four = float32 4 }, t)
+
+[<Fact>]
+let ``Test2 create Test`` () =
+    let t = Test2.Test1.create 1 "2" 3. (float32 4)
+    Assert.Equal({Test1.one = 1; two = "2"; three = 3.; four = float32 4 }, t)
+
+[<Fact>]
+let ``Test1 accessor Test`` () =
+    let t = Test.Test1.create 1 "2" 3. (float32 4)
+    let z = Test.Test1.one t
+    Assert.Equal(1, z)
+
+[<Fact>]
+let ``Test2 accessor Test`` () =
+    let t = Test2.Test1.create 1 "2" 3. (float32 4)
+    let z = Test2.Test1.one t
+    Assert.Equal(1, z)
