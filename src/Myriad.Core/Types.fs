5a3f2038b0a7f96a7ba5f9f93c72bd8210af097b
Dave Thomas <kukulcanenator@googlemail.com>
2020-12-19 14:16:06 +0000

    Merge conflict weirdness

--- src/Myriad.Core/Types.fs
+++ src/Myriad.Core/Types.fs
@@ -3,8 +3,9 @@ namespace Myriad.Core
 open System
 open FSharp.Compiler.SyntaxTree
 
-type MyriadGeneratorAttribute() =
+type MyriadGeneratorAttribute(name: string) =
     inherit Attribute()
+    member __.Name = name
 
 type IMyriadGenerator =
     abstract member ValidInputExtensions: string seq
