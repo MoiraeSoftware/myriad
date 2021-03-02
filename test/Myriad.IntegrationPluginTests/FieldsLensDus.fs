3f980046432877405602a10fb3175df54e1ff98e
7sharp9 <kukulcanenator@googlemail.com>
2021-03-02 12:36:28 +0000

    Update FieldsLensDus.fs

--- test/Myriad.IntegrationPluginTests/FieldsLensDus.fs
+++ test/Myriad.IntegrationPluginTests/FieldsLensDus.fs
@@ -146,18 +146,19 @@ module Test1 =
     let two (x: Test1) = x.two
     let three (x: Test1) = x.three
     let four (x: Test1) = x.four
-    let create (one: int) (two: string) (three: float) (four: float32): Test1 =
+    let create (one: int) (two: string) (three: float) (four: float32) : Test1 =
         { one = one
           two = two
           three = three
           four = four }
 
-    let map (mapone: int -> int)
-            (maptwo: string -> string)
-            (mapthree: float -> float)
-            (mapfour: float32 -> float32)
-            (record': Test1)
-            =
+    let map
+        (mapone: int -> int)
+        (maptwo: string -> string)
+        (mapthree: float -> float)
+        (mapfour: float32 -> float32)
+        (record': Test1)
+        =
         { record' with
               one = mapone record'.one
               two = maptwo record'.two
