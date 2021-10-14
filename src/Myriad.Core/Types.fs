c0acac2c4f5b08806588d60f4cb208ac04d2a454
7sharp9 <kukulcanenator@googlemail.com>
2021-10-14 18:50:22 +0100

    Added helper create
    dogfood?

--- src/Myriad.Core/Types.fs
+++ src/Myriad.Core/Types.fs
@@ -8,13 +8,23 @@ type MyriadGeneratorAttribute(name: string) =
     inherit Attribute()
     member _.Name = name
 
-type GeneratorContext = {
-    ConfigKey: string option
-    ConfigGetter: string -> (string * obj) seq
-    InputFilename: string
-    AdditionalParameters: IDictionary<string, string>
-}
+type GeneratorContext =
+    {   ConfigKey: string option
+        ConfigGetter: string -> (string * obj) seq
+        InputFilename: string
+        AdditionalParameters: IDictionary<string, string> }
+    
+    static member Create(configKey, configHandler, inputFile, additionalParams) =
+        { ConfigKey = configKey
+          ConfigGetter = configHandler
+          InputFilename = inputFile
+          AdditionalParameters = additionalParams }
+
+[<RequireQualifiedAccess>]
+type Output =
+    | Ast of SynModuleOrNamespace list
+    | Source of string
 
 type IMyriadGenerator =
     abstract member ValidInputExtensions: string seq
-    abstract member Generate: GeneratorContext -> SynModuleOrNamespace list
+    abstract member Generate: GeneratorContext -> Output
